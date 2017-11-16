8a0ebd2820c4e4abda6f8bc0e13445f5a8d2bb3e
garinheil <heil.garin@gmail.com>
2017-11-15 22:57:52 -0500

    Remove an import

--- app/src/main/java/heil1gd/cps496/cmich/edu/finalproject/MainActivity.java
+++ app/src/main/java/heil1gd/cps496/cmich/edu/finalproject/MainActivity.java
@@ -8,7 +8,6 @@ package heil1gd.cps496.cmich.edu.finalproject;
  *
  */
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
